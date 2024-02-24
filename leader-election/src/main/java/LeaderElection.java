d926f9c253967c821d14acd928d53ff2da3d1292
ashutoshgunjal <ashutosh.r.gunjal@gmail.com>
2024-02-24 08:02:00 -0500

    Leader Election Distributed Algorithm

--- leader-election/src/main/java/LeaderElection.java
+++ leader-election/src/main/java/LeaderElection.java
@@ -1,12 +1,14 @@
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 public class LeaderElection implements Watcher {
     private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
     private static final int SESSION_TIMEOUT = 5000;
+    private static final String ELECTION_NAMESPACE = "/election";
+    private String currentZnodeName;
     private ZooKeeper zookeeper;
 
     public void connectToZookeeper() throws IOException {
@@ -40,9 +42,63 @@ public class LeaderElection implements Watcher {
         }
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    /**
+     * Volunteer for leadership by creating a znode using the candidate leadership method.
+     *
+     * In this method:
+     * - Znode prefix is set to denote candidacy.
+     * - ZooKeeper's create method is invoked, specifying:
+     *   - Znode prefix for identification.
+     *   - Empty byte array as the data to be stored in the znode.
+     *   - Open unsafe access control list (ACL) for unrestricted access.
+     *   - Znode creation mode set to ephemeral sequential, ensuring automatic deletion upon disconnection.
+     *
+     * The return value from ZooKeeper contains the full path of the created znode.
+     * Extract and store this information in a class member variable.
+     *
+     * @throws InterruptedException if the thread is interrupted while waiting for a response.
+     * @throws KeeperException      if a ZooKeeper operation fails.
+     */
+
+    public void volunteerForLeadership() throws InterruptedException, KeeperException {
+        String znodePrefix = ELECTION_NAMESPACE + "/c_";
+        String znodeFullPath = zookeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        System.out.println("Znode Name: " + znodeFullPath);
+        this.currentZnodeName = znodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
+    }
+
+    /**
+     * Elects a leader using the volunteer leadership method.
+     *
+     * In this method:
+     * - Retrieves the list of children znodes under the specified namespace using ZooKeeper's getChildren method.
+     * - Sorts the list of children znodes to determine the smallest znode.
+     * - Compares the current znode name with the smallest znode in the list.
+     * - Prints a message indicating whether the current znode is the leader or not.
+     *
+     * @throws InterruptedException if the thread is interrupted while waiting for a response.
+     * @throws KeeperException      if a ZooKeeper operation fails.
+     */
+
+    public void electLeader() throws InterruptedException, KeeperException {
+        List<String> children = zookeeper.getChildren(ELECTION_NAMESPACE, false);
+
+        Collections.sort(children);
+        String smallestChild = children.get(0);
+
+        if (smallestChild.equals(currentZnodeName)) {
+            System.out.println("I am the leader znode");
+            return;
+        }
+        System.out.println("I ma not a leader znode, " + smallestChild + " is the leader");
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         LeaderElection leaderElection = new LeaderElection();
         leaderElection.connectToZookeeper();
+        leaderElection.volunteerForLeadership();
+        leaderElection.electLeader();
         leaderElection.run();
         leaderElection.close();
         System.out.println("Disconnected from Zookeeper, exiting the application");
